18cde2779e4c8217dd5fef57d29185a7aa9f2d89
komo-minata <minanatakomo@gmail.com>
2023-07-25 11:55:46 +0800

    header

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+const header = document.querySelector("header");
+// console.log(nav);
+window.addEventListener("scroll", () => {
+   console.log(window.scrollY);
+   
+   if (window.scrollY > 120){
+      header.style.top = 0;
+   } else{
+      header.style.top = "-50px"
+   }
+});
+
+
+let toggle = document.querySelector(".toggle");
+let body = document.querySelector("body");
+
+
+
+toggle.addEventListener("click", function ()  {
+    body.classList.toggle("open");
+});
---
+++ k.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector("nav");
+// console.log(nav);
+window.addEventListener("scroll", () => {
+   console.log(window.scrollY);
+   
+   if (window.scrollY > 120){
+      nav.style.top = 0;
+   } else{
+      nav.style.top = "-50px"
+   }
+});
